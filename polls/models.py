ae21b7d0748e81bed16bf130163309a863593671
liawom@foxmail.com <60684451+aw0m@users.noreply.github.com>
2021-05-20 02:24:49 +0800

    添加了判断提问时间

--- polls/models.py
+++ polls/models.py
@@ -1,3 +1,6 @@
+import datetime
+from django.utils import timezone
+
 from django.db import models
 
 # Create your models here.
@@ -5,7 +8,10 @@ from django.db import models
 
 class Question(models.Model):
     question_text = models.CharField(max_length=200)
-    pub_time = models.DateTimeField('date published!')
+    pub_date = models.DateTimeField('date published!')
+
+    def was_published_recently(self):
+        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
 
     def __str__(self):
         return self.question_text
