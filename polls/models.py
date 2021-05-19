45c8a3cffa0f83b4d7eb95b0b2c4c7a9bf3f94eb
liawom@foxmail.com <60684451+Aw0m@users.noreply.github.com>
2021-05-20 01:59:54 +0800

    新建的Models

--- polls/models.py
+++ polls/models.py
@@ -5,7 +5,7 @@ from django.db import models
 
 class Question(models.Model):
     question_text = models.CharField(max_length=200)
-    pub_time = models.DateTimeField('date published')
+    pub_time = models.DateTimeField('date published!')
 
     def __str__(self):
         return self.question_text
