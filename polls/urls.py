9294c2eb4db341e75d1a36877375dfbf68c5fac1
liawom@foxmail.com <60684451+aw0m@users.noreply.github.com>
2021-05-21 18:41:37 +0800

    根据相应的请求展示不同的界面

--- polls/urls.py
+++ polls/urls.py
@@ -3,8 +3,18 @@ from django.urls import path
 from . import views
 
 """
-接收到剩余的url路径之后，进行判断并调用views.py 中的index函数，显示界
+接收到剩余的url路径之后，进行判断并调用views.py 中的index函数，显示界面。
+name为URL指定了名字，方便显示
+app_name可以作为命名空间，用以帮助Django分辨重名的URL
 """
+app_name = 'polls'
 urlpatterns = [
-    path('hello', views.index, name='index'),
+    # ex: /polls/
+    path('', views.index, name='index'),
+    # ex: /polls/5/
+    path('<int:question_id>/', views.detail, name='detail'),
+    # ex: /polls/5/results/
+    path('<int:question_id>/results/', views.results, name='results'),
+    # ex: /polls/5/vote/
+    path('<int:question_id>/vote/', views.vote, name='vote')
 ]
--- polls/views.py
+++ polls/views.py
@@ -1,5 +1,39 @@
 from django.http import HttpResponse
+from django.http import Http404
+from django.shortcuts import render, get_object_or_404
+from .models import Question
 
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    # order_by(xxx)         升序排序
+    # order_by(-xxx)        降序排序
+    # 均返回一个 QuerySet[T]
+    # 通过切片获得最近的5个Question来显示
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    context = {
+        'latest_question_list': latest_question_list,
+    }
+    return render(request, 'index.html', context)
+
+
+def detail(request, question_id):
+    """
+    通过get_object_or_404而并非如下的
+    try:
+        question = Question.objects.get(pk=question_id)
+    except Question.DoesNotExist:
+        raise Http404("Question does not exit")
+    return render(request, 'detail.html', {'question': question})
+    来自己捕获 ObjectDoesNotExist 异常，以及抛出不直接抛出 ObjectDoesNotExist 而是抛出 Http404 ，是为了降低模型层和视图层的耦合性
+    """
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'detail.html', {'question': question})
+
+
+def results(request, question_id):
+    response = "You're looking at the results of question %s."
+    return HttpResponse(response % question_id)
+
+
+def vote(request, question_id):
+    return HttpResponse("You're voting on question %s." % question_id)
